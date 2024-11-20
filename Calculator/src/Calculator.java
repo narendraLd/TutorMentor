9ea0656439beb263db91c02c605b8e3f18dc42b4
narendraLd <narendra.sahu@ldtech.in>
2024-11-20 20:10:27 +0530

    again i changes to + symob

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,6 +1,6 @@
 public class Calculator {
     public static int sum(int a ,int b){
-        return a-b;
+        return a+b;
     }public static int sub(int a ,int b){
         return a-b;
     }public static int mul(int a ,int b){
